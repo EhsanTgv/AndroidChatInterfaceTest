1b0d75b6b5a4a22ce0fc212fa6226a471b9eda68
Ehsan Taghavi <ehsantgv@gmail.com>
2020-11-15 18:30:09 +0330

    MainActivity recycler adapter is implemented

--- app/src/main/java/com/taghavi/androidchatinterface/MainActivity.kt
+++ app/src/main/java/com/taghavi/androidchatinterface/MainActivity.kt
@@ -2,10 +2,31 @@ package com.taghavi.androidchatinterface
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.taghavi.androidchatinterface.models.Message
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.kotlinandroidextensions.GroupieViewHolder
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val messageAdapter = GroupAdapter<GroupieViewHolder>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        recyclerView.adapter = messageAdapter
+        populateData()
+    }
+
+    private fun populateData() {
+        val data = listOf<Message>()
+        data.forEach {
+            if (it.sendBy == "me") {
+                messageAdapter.add(SendMessageItem(it))
+            } else {
+                messageAdapter.add(ReceiveMessageItem(it))
+            }
+        }
     }
 }
